4fdd8e42ad62a868a64e94a71481a7ce393b1460
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-12-12 20:27:15 -0800

    Update gmes.ts

--- src/utils/gmes.ts
+++ src/utils/gmes.ts
@@ -1,5 +1,4 @@
 import gmesData from "@/data/gmes.json";
-import { fetch } from "@/utils/fetch.ts";
 
 const FILTER_OPTIMIZE_ON = import.meta.env.PUBLIC_FILTER_OPTIMIZE === "true";
 const gmes_text = FILTER_OPTIMIZE_ON ? "gᾰmes" : "games";
